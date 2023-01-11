2f03b59824184ffb218a8c8f610f2af9e102a13a
Vlad Melekhin <vmeduc@yandex.ru>
2023-01-11 12:42:36 +0300

    reload

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -3,9 +3,7 @@ self.addEventListener("install", function () {
 });
 
 self.addEventListener("push", function (event) {
-  if (event.data) {
-    console.log("This push event has data: ", event.data.text());
-  } else {
-    console.log("This push event has no data.");
-  }
+  const promiseChain = self.registration.showNotification("Hello, World.");
+
+  event.waitUntil(promiseChain);
 });
