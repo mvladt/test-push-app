fdc880b8ed0c2b023daf4cd3eb0bba34096bf2ba
Vlad Melekhin <vmeduc@yandex.ru>
2023-01-11 12:37:00 +0300

    reload

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,3 +1,11 @@
 self.addEventListener("install", function () {
   console.log("service-worker.js 'install'");
 });
+
+self.addEventListener("push", function (event) {
+  if (event.data) {
+    console.log("This push event has data: ", event.data.text());
+  } else {
+    console.log("This push event has no data.");
+  }
+});
